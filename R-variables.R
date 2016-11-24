78c142d3b61cef7f003ffe3294651603c6dae8da
SamikshaAg <samiksha71295@gmail.com>
2016-11-24 23:41:31 +0530

    Add files via upload

---
+++ R-variables.R
@@ -0,0 +1,16 @@
+# A numeric variable
+x <- 5
+# A character variable 
+y <- "America"
+# A complex variable
+z <- 8+9i
+
+#printing the values of the variables
+x
+y
+z
+
+# Alternative way to print the value
+print(x)
+print(y)
+print(z)
